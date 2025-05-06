672983757472469921b4af017b578ce1d73862c6
noobissk <noobissk@gmail.com>
2025-05-06 11:52:38 +0200

    hero fix

--- Assets/Scripts/Heroes/Hero.cs
+++ Assets/Scripts/Heroes/Hero.cs
@@ -5,26 +5,51 @@ public abstract class Hero : MonoBehaviour
     public int health;
     public int maxHealth;
     public float movementSpeed;
+
     public float minDistanceFromPlayer;
     public float maxDistanceFromPlayer;
 
+    public bool isIdealDistance { get { float f = _distanceFromPlayer; return f > minDistanceFromPlayer && f < maxDistanceFromPlayer; } }
+
+    protected float _distanceFromPlayer { get { return Vector2.Distance(Boss.Transform.position, transform.position); } }
+    protected Vector2 _directionToPlayer { get { return (Boss.Transform.position - transform.position).normalized; } }
     private Rigidbody rb;
 
+    public float maxStamina;
+    public float actionStaminaCost;
     [Header("Hero specific values")]
-    private float stamina;
+    private float _stamina;
 
 
-    protected void MoveToTarget()
+    void FixedUpdate()
     {
-        if (Boss.Transform = )
+        _stamina = Mathf.Clamp(_stamina + Time.fixedDeltaTime, 0, maxStamina);
+        if (_stamina > actionStaminaCost && isIdealDistance)
+        {
+            Action();
+            _stamina -= actionStaminaCost;
+        }
     }
-    protected void Action()
-    {
 
+
+    protected void MoveToPreferredDistance()
+    {
+        float dist = _distanceFromPlayer;
+        if (dist < minDistanceFromPlayer)
+            rb.AddForce(-_directionToPlayer * movementSpeed * rb.mass);
+        else if (dist > maxDistanceFromPlayer)
+            rb.AddForce(_directionToPlayer * movementSpeed * rb.mass);
+    }
+    protected virtual void Action()
+    {
+        // hero specific
     }
 
     public void EditHealth(int value)
     {
-        // TODO: health stuff
+        health = Mathf.Clamp(health + value, 0, maxHealth);
+
+        if (health == 0)
+            Destroy(gameObject); // gameOver
     }
 }
--- Assets/Scripts/Heroes/Knight.cs
+++ Assets/Scripts/Heroes/Knight.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class Knight : Hero
 {
-    
+    // public float 
+    public Collider2D damageCol;
+    private void Start()
+    {
+        damageCol.gameObject.SetActive(false);
+    }
+    protected override void Action()
+    {
+        base.Action();
+        // var cols = Physics2D.OverlapBox();
+    }
 }
