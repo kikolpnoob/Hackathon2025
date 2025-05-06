0b42ff0091ce959c2596d03faa0a178a43f6b31b
macoo88 <macgab88@gmail.com>
2025-05-06 12:40:18 +0200

    minionPrefab& & summonMinion script

--- Assets/Scripts/Boss/SummonMinions.cs
+++ Assets/Scripts/Boss/SummonMinions.cs
@@ -1,9 +1,12 @@
+using UnityEngine;
 public class SummonMinions : Ability
 {
+    public GameObject MinionPrefab;
     public override void UseAbility()
     {
         base.UseAbility();
-        
+
+        Object.Instantiate(MinionPrefab, Boss.Transform.position, Quaternion.identity);
     }
     
     
